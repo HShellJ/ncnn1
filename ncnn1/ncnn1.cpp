10fb30ca1e0e381957b2a441385e37cac00f00a0
25190 <25190@HSRMACH>
2025-09-27 20:35:36 +0800

    添加项目文件。

---
+++ ncnn1/ncnn1.cpp
@@ -0,0 +1,42 @@
+﻿// ncnn1.cpp: 定义应用程序的入口点。
+//
+
+#include "ncnn1.h"
+
+using std::cout;
+using std::endl;
+using ncnn::Net;
+using ncnn::Mat;
+
+Mat prepare_input(int x) {
+	// 创建1维输入（长度为1），值为x（若训练时有归一化，需同步处理，如x/1000.0）
+	Mat input(1);
+	input[0] = (float)x;
+	return input;
+}
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	Net net;
+
+	net.load_param("C:\\Users\\25190\\Documents\\Model\\function_fitting_model.ncnn.param");
+	net.load_model("C:\\Users\\25190\\Documents\\Model\\function_fitting_model.ncnn.bin");
+
+
+    for (int x = 1; x <= 1000; x++) {
+        // 准备1维输入
+        Mat input = prepare_input(x);
+
+        ncnn::Extractor ex = net.create_extractor();
+        ex.input("in0", input);
+
+        Mat output;
+        ex.extract("out0", output);
+
+        float result = output[0];
+        std::cout << "输入: " << x << ", 推理结果: " << result << std::endl;
+    }
+
+	return 0;
+}
---
+++ ncnn1/ncnn1.h
@@ -0,0 +1,11 @@
+﻿// ncnn1.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+#include "ncnn/net.h"
+#include "opencv2/opencv.hpp"
+#include <cmath>
+
+// TODO: 在此处引用程序需要的其他标头。
